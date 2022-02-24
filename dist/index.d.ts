07caf24e0f17c92c9674fb70ed3003a4ee2c97f2
Sergio Herrero <herrerocontact@gmail.com>
2022-02-24 20:50:19 +0100

    Removing the build fild from gitignore so it is part of the published package.

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+declare function main(): void;
+declare function testFunction(): void;
+export interface ISomeRandomInterfaceExport {
+    one: string;
+    two: string;
+}
+export declare type Hello = string | number;
+export { main, testFunction };
---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testFunction = exports.main = void 0;
+function main() {
+    console.log('Hello world from the main exported function!');
+}
+exports.main = main;
+function testFunction() {
+    console.log('Hello world from the main exported function!');
+}
+exports.testFunction = testFunction;
