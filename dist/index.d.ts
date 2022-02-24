d790d1ed90c7359a7dde9adb6d276bc24ed43124
Sergio Herrero <herrerocontact@gmail.com>
2022-02-24 20:57:33 +0100

    Changing npm config to include the build folder.

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare function main(): void;
-declare function testFunction(): void;
-export interface ISomeRandomInterfaceExport {
-    one: string;
-    two: string;
-}
-export declare type Hello = string | number;
-export { main, testFunction };
