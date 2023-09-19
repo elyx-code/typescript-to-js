a393d8550f0e08cf6adac2b934b8694470c2c7ec
Sergio Herrero <34370388+sergioh213@users.noreply.github.com>
2023-09-19 10:32:50 +0200

    Fix uninit properties (#1)
    * New third party compiler engine and new tests
    
    * Cleaned up code

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,31 @@
+import { readFileSync, readdirSync } from 'fs';
+import path from 'path';
+import { compile } from './index';
+
 describe('test', () => {
-  test('add', async () => {
-    expect(1 + 1).toEqual(2);
-  });
+  // Read the files in the test folder and loop over them
+  // We then dynamically read the contents of each file and run it as a test
+
+  let testFolder;
+
+  if (__dirname.includes('dist')) {
+    testFolder = path.join(__dirname, '..', '..', 'tests', 'compilation-test-files');
+  } else {
+    testFolder = path.join(__dirname, '..', 'tests', 'compilation-test-files');
+  }
+  const files = readdirSync(testFolder);
+
+  for (let index = 0; index < files.length; index++) {
+    const file = files[index];
+    const fileContents = readFileSync(path.join(testFolder, file), 'utf8');
+
+    test(`Test compilation of demo file: ${file}`, async () => {
+      try {
+        const result = compile(fileContents);
+        expect(true).toEqual(true);
+      } catch (error) {
+        expect(true).toEqual(false);
+      }
+    });
+  }
 });
