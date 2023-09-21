79a19e1962c6e40dde1c192dc412aa35ecf1cca7
Sergio Herrero <herrerocontact@gmail.com>
2023-09-21 22:58:20 +0200

    Improved package definition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// import AbstractSyntaxTree from 'abstract-syntax-tree';
 import { format, FormattingOptions } from 'format-javascript';
-// DON"T CHANGE: Must be a "require" and not an "import"
+// DON'T CHANGE: Must be a "require" and not an "import"
 const compiler = require('@babel/core');
+const toTypescript = require('@babel/plugin-transform-typescript');
 
 function isObject(obj: any) {
   return typeof obj === 'object' && obj !== null
@@ -75,7 +75,8 @@ function recursivelyDeleteTSElementsFromAST(tsASTSection: ReturnType<any>) {
 
 function compile(tsCodeString: string, formattingOptions?: FormattingOptions) {
   const compiled = compiler.transformSync(tsCodeString, {
-    plugins: ['@babel/plugin-transform-typescript'],
+    plugins: [toTypescript],
+    babelrc: false,
   });
 
   const formattedJsCode = format(compiled?.code as string, {
@@ -85,39 +86,6 @@ function compile(tsCodeString: string, formattingOptions?: FormattingOptions) {
   });
 
   return formattedJsCode;
-  // // Pass the TS code to the 3rd party parser to turn it into a compliant TS abstract syntax tree (AST)
-  // // @ts-ignore
-  // const typescriptAST = parse(tsCodeString, {
-  //   sourceType: 'module',
-  //   range: true
-  // });
-
-  // // Recursively read that AST object and drop all the TS elements to leave only the JS ones
-  // const sanitizedAst = recursivelyDeleteTSElementsFromAST(typescriptAST);
-
-  // const jsAST = new AbstractSyntaxTree();
-
-  // // Looping root list of elements in the TS tree and appending them to the new and empty JS tree
-  // for (let index = 0; index < sanitizedAst.body.length; index++) {
-  //   const element: any = sanitizedAst.body[index];
-  
-  //   AbstractSyntaxTree.append(jsAST, element);
-  // }
-  
-  // // Take the newly created JS AST and use a 3rd party code generator to output a string of JS code
-  // const unformattedJsCode = AbstractSyntaxTree.generate(jsAST);
-
-  // // Format the output code with a 3rd party code formatter
-  // const formattedJsCode = format(
-  //   unformattedJsCode,
-  //   {
-  //     indent_size: 2,
-  //     space_in_empty_paren: true,
-  //     ...formattingOptions
-  //   }
-  // );
-
-  // return formattedJsCode;
 }
 
 export {
